4fe4da07d3d69050d9034d3ae0393f50266da83e
Iran Monroy <iranxmon@gmail.com>
2023-01-10 16:21:02 -0600

    creando menu

---
+++ calculadora2.py
@@ -0,0 +1,18 @@
+opcion = "si"
+
+while opcion != "no":
+    print(""" ><Calculadora><
+          
+          Operacion a realizar:"
+          
+          1) Sumar
+          2) Restar 
+          3) Multiplicar 
+          4) Dividir
+          5) Cambiar los numeros introducidos
+          6) Salir d ela calculadora 
+          
+          """)
+    opcion = int(input("Ingresa la operacion:"))
+    
+   
