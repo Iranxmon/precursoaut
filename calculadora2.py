10472a05e1dec056275f0617fd5d82aad349a25c
Iran Monroy <iranxmon@gmail.com>
2023-01-10 16:41:35 -0600

    creando primeras operaciones

--- calculadora2.py
+++ calculadora2.py
@@ -9,10 +9,29 @@ while opcion != "no":
           2) Restar 
           3) Multiplicar 
           4) Dividir
-          5) Cambiar los numeros introducidos
-          6) Salir d ela calculadora 
+          5) Raiz
+          6) Exponente
+          7) Seno
+          8) Coseno
+          9) Tangente
+          10) Salir de la calculadora 
           
           """)
+    
     opcion = int(input("Ingresa la operacion:"))
+    
+    if opcion >= 10:
+        print("gracias por usar mi calculadora")
+        opcion = "no"
+        
+    else:
+        x=int(input("Ingrese el primer numero para calcular: "))
+        y=int(input("Ingrese el segundo numero para calcular: "))
+        if opcion==1:
+            print("la suma es ", x+y)
+    
+
+        opcion = input("Desea realizar otra operacion 'si' o 'no': ")
+   
     
    
