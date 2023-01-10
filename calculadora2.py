09519ce6baade844d239307ffe5fb3e55a67f932
Iran Monroy <iranxmon@gmail.com>
2023-01-10 17:08:40 -0600

    agregando funciones trigonometricas

--- calculadora2.py
+++ calculadora2.py
@@ -1,3 +1,5 @@
+from math import sqrt, sin , cos , tan
+
 opcion = "si"
 
 while opcion != "no":
@@ -26,15 +28,30 @@ while opcion != "no":
         
     else:
         x=int(input("Ingrese el primer numero para calcular: "))
-        y=int(input("Ingrese el segundo numero para calcular: "))
+        
         if opcion==1:
+            y=int(input("Ingrese el segundo numero para calcular: "))
             print("la suma es ", x+y)
         elif opcion==2:
+            y=int(input("Ingrese el segundo numero para calcular: "))
             print("la resta es ", x-y)
         elif opcion==3:
+            y=int(input("Ingrese el segundo numero para calcular: "))
             print("la multiplicacion es ", x*y)
         elif opcion==4:
+            y=int(input("Ingrese el segundo numero para calcular: "))
             print("la division es ",x/y)
+        elif opcion==5:
+            print("la raiz es ",sqrt(x))
+        elif opcion==6:
+            y=int(input("Ingrese el segundo numero para calcular: "))
+            print("El exponente es ",x**y)
+        elif opcion==7:
+            print("El seno es ",sin(x))
+        elif opcion==8:
+            print("El coseno es ",cos(x))
+        elif opcion==9:
+            print("El tangente es ",tan(x))
     
 
         opcion = input("Desea realizar otra operacion 'si' o 'no': ")
